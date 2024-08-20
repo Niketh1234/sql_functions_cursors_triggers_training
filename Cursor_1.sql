54044b0cf706e8ff24c746c00e4bcdb44096aa0f
dniketh <dniketh@actalentservices.com>
2024-08-20 08:11:47 +0000

    Added the cursor files

---
+++ Cursor_1.sql
@@ -0,0 +1,32 @@
+create table cart(itemcode int,name varchar(30), qty int,price float)
+insert into cart(itemcode,name,qty,price) values
+(101,'iphone',1,399.00),
+(201,'realme',2,999.00),
+(301,'samsung',3,1999.00),
+(401,'micromax',9,99.00)
+
+select * from cart
+
+alter table cart drop column slno
+
+alter table cart add slno int
+
+alter procedure updateslnoCart
+as 
+begin
+	declare carts cursor for select * from cart
+	declare @itemcode int
+	declare @name varchar(30)
+	declare @qty int
+	declare @price float
+	declare @slno int
+	open carts
+	fetch carts into @itemcode,@name,@qty,@price,@slno
+	while(@@FETCH_STATUS != -1) -- while(@@FETCH_STATUS = 0)
+		begin
+			print @itemcode
+			fetch carts into @itemcode,@name,@qty,@price,@slno
+		end 
+	close carts
+	deallocate carts
+end
